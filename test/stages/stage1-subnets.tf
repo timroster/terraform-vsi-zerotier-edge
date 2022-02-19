bb8892144f8517c36a7065cae95c8fb655d643e7
Tim Robinson <timroster@gmail.com>
2022-02-18 17:55:50 -0800

    upstream module whack-a-mole

--- test/stages/stage1-subnets.tf
+++ test/stages/stage1-subnets.tf
@@ -1,7 +1,7 @@
 module "subnets" {
   source = "github.com/cloud-native-toolkit/terraform-ibm-vpc-subnets.git"
 
-  resource_group_id = module.resource_group.id
+  resource_group_name = var.resource_group_name
   region            = var.region
   vpc_name          = module.vpc.name
   gateways          = module.gateways.gateways
